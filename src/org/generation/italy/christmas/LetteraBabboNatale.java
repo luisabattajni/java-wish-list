ded69c66c266b5a23630e9f1bc7207f5999e2a9e
luisabattajni <luisasara.battajni@gmail.com>
2021-12-01 17:41:44 +0100

    wip

--- src/org/generation/italy/christmas/LetteraBabboNatale.java
+++ src/org/generation/italy/christmas/LetteraBabboNatale.java
@@ -1,19 +1,50 @@
 package org.generation.italy.christmas;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class LetteraBabboNatale {
 	
 
-	
 	//attributi
-	
-//	nome (String)
-//	indirizzo (String)
-//	listaDesideri (List<String>)
-
 	private String nome;
 	private String indirizzo;
+	private List<String> listaDesideri;
+	
+	/*
+	 * 
+	 * Implementare un opportuno costruttore e il metodo invia():String.
+	Il metodo deve controllare che la lunghezza della lista 
+	non superi la lunghezza massima consentita (5 desideri): 
+	se la supera il metodo lancia un’eccezione, 
+	altrimenti ritorna una stringa contenente un recap 
+	di tutte le informazioni(nome, indirizzo, lista).
+	
+	Nel metodo main, in coda al programma già implementato, 
+	chiedere all’utente il nome e l’indirizzo a cui inviare i regali. 
+	Con quei parametri e con la lista dei desideri già creata 
+	istanziare un nuovo oggetto di tipo LetteraBabboNatale.
+	Chiamare il metodo invia() e stampare a video l’esito dell’invio, 
+	gestendo opportunamente eventuali eccezioni.
+	 */
+	
+	// costruttore
+	
+//	public LetteraBabboNatale (String nome, String indirizzo) {
+//		this.nome = nome;
+//		this.indirizzo = indirizzo;
+//		this.listaDesideri = new List<String>();
+//	}
+	
+	public LetteraBabboNatale (String nome, String indirizzo, List<String> listaDesideri ) {
+		this.nome = nome;
+		this.indirizzo = indirizzo;
+		this.listaDesideri = listaDesideri;
+	}
+	
+
+
+	
+	
 
 
 }
